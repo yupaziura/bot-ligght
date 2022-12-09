b16cc0fb73c7a1f16f36fe3cca00f9307ccdf5f9
YuliiaPaziura <yuliiapaziura@gmail.com>
2022-12-09 15:20:29 +0200

    Edit bot logic

--- app.js
+++ app.js
@@ -4,46 +4,26 @@ import { Telegraf } from 'telegraf';
 import pkg from 'express';
 import got from 'got';
 import https from 'https';
+import fetch from "node-fetch";
 
 
 const app = express();
 const bot = new Telegraf(TOKEN)
 
-async function isAvailable() {
-    var request =   got.get(`https://vadymklymenko.com/ping/?ip=109.251.102.171`)
-    request.headers = {"Accept": "application/json"}
-  
-    let response = await JSON.parse(JSON.stringify(request))
-  
-    // Scriptable will throw an error to widget in case of internet issues
-    return [response["status"] == "ok", response["error"]]
-  }
-
-// async function isAvailable() {
-//     const options = {
-//         hostname: 'https://vadymklymenko.com/ping/?ip=109.251.102.171',
-//         // port: 443,
-//         // path: '/',
-//         method: 'GET',
-//         headers: {
-//            'Content-Type': 'application/json',
-//            'User-Agent': 'Foo'
-//         }
-//       };
-
-   
-
-//   }
 
 
 bot.start(async ctx => {
-    const [isWifiAvailable, errorText] = await isAvailable(); 
-    ctx.reply(isWifiAvailable? 'ye' : 'nema')
-    // const test = await isAvailable(); 
-    // ctx.reply(test)
+
+    fetch('https://vadymklymenko.com/ping/?ip=109.251.102.171')
+    .then((response) => response.json())
+    .then((data) => {
+        // console.log(data.status.toString())
+        ctx.reply(`${data.status == 'ok' ? "ye" : "nema"}`)
+
+        // return data.status 
+    
+    });
 })
-// bot.on('text', ctx => {
-//     ctx.reply('just text')
-// })
+
 bot.launch()
 app.listen(PORT, () => console.log(`My server is running on port ${PORT}`))
